690afc39f9792b09bd5ca1c5ca017e543a148e5f
Titis Esti Rahayu <titisestirhy14@gmail.com>
2022-10-26 18:19:08 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello World!<br>";
+
+$nama = "Titis Esti";
+$umur = 21;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Delisa";
+$umurAdik = 3;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun.";
+
+?>
